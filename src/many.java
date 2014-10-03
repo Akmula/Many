45bbeb16eebe57850d6949563bd8960a065eab24
Akmula <akmula@mail.ru>
2014-10-03 13:25:24 +0400

    Update many.java

--- src/many.java
+++ src/many.java
@@ -5,25 +5,24 @@ public class many {
         int[] x = new int[5];
         int[] y = new int[3];
         int[] z = new int[x.length + y.length];
+        Random rnd = new Random();
         System.out.print("X = { ");
         for (int i = 0; i < x.length; i++) {
-            x[i] = (new Random().nextInt(10));
+            x[i] = rnd.nextInt(10);
             System.out.print(x[i] + " ");
         }
         System.out.println("}");
         System.out.print("Y = { ");
         for (int i = 0; i < y.length; i++) {
-            y[i] = (new Random().nextInt(10));
+            y[i] = rnd.nextInt(10);
             System.out.print(y[i] + " ");
         }
         System.out.println("}");
         System.arraycopy(x, 0, z, 0, x.length);
         System.arraycopy(y, 0, z, x.length, y.length);
         System.out.print("Z = { ");
-        int i = 0;
-        while (i < z.length) {
+        for (int i = 0; i < z.length; i++) {
             System.out.print(z[i] + " ");
-            i++;
         }
         System.out.print("}");
     }
